0329be9e370142bb227e74259e75a769d788967c
Shareen <112009779+Shareencoco@users.noreply.github.com>
2026-07-30 21:00:40 +0530

    Add data transformation functions for training and validation

---
+++ f2mamba/datasets/transforms.py
@@ -0,0 +1,37 @@
+import albumentations as A
+
+from albumentations.pytorch import ToTensorV2
+
+
+def get_train_transforms(image_size=512):
+
+    return A.Compose(
+        [
+            A.Resize(image_size, image_size),
+
+            A.HorizontalFlip(p=0.5),
+
+            A.Normalize(
+                mean=(0.485,0.456,0.406),
+                std=(0.229,0.224,0.225)
+            ),
+
+            ToTensorV2()
+        ]
+    )
+
+
+def get_val_transforms(image_size=512):
+
+    return A.Compose(
+        [
+            A.Resize(image_size,image_size),
+
+            A.Normalize(
+                mean=(0.485,0.456,0.406),
+                std=(0.229,0.224,0.225)
+            ),
+
+            ToTensorV2()
+        ]
+    )
