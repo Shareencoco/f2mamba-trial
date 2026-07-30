e51b54887b92dbec4102da57353dce2c5b5dca4f
Shareen <112009779+Shareencoco@users.noreply.github.com>
2026-07-30 21:16:55 +0530

    Refactor IMD2020Dataset to accept optional image list

--- f2mamba/datasets/imd2020.py
+++ f2mamba/datasets/imd2020.py
@@ -1,7 +1,5 @@
 import os
-
 import cv2
-
 import numpy as np
 
 from torch.utils.data import Dataset
@@ -13,62 +11,45 @@ class IMD2020Dataset(Dataset):
         self,
         image_dir,
         mask_dir,
-        transforms=None
+        image_list=None,
+        transforms=None,
     ):
-
         self.image_dir = image_dir
-
         self.mask_dir = mask_dir
-
         self.transforms = transforms
 
-        self.images = sorted(os.listdir(image_dir))
-
+        if image_list is None:
+            self.images = sorted(os.listdir(image_dir))
+        else:
+            self.images = image_list
 
     def __len__(self):
-
         return len(self.images)
 
+    def __getitem__(self, idx):
 
-    def __getitem__(self,index):
+        image_name = self.images[idx]
 
-        image_name = self.images[index]
-
-        image_path = os.path.join(
-            self.image_dir,
-            image_name
-        )
-
-        mask_path = os.path.join(
-            self.mask_dir,
-            image_name
-        )
+        image_path = os.path.join(self.image_dir, image_name)
+        mask_path = os.path.join(self.mask_dir, image_name)
 
         image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        image = cv2.cvtColor(
-            image,
-            cv2.COLOR_BGR2RGB
-        )
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
 
-        mask = cv2.imread(
-            mask_path,
-            cv2.IMREAD_GRAYSCALE
-        )
-
-        mask = (mask>127).astype(np.float32)
+        # binary mask
+        mask = (mask > 127).astype(np.float32)
 
         if self.transforms:
-
-            aug = self.transforms(
+            transformed = self.transforms(
                 image=image,
                 mask=mask
             )
 
-            image = aug["image"]
-
-            mask = aug["mask"]
+            image = transformed["image"]
+            mask = transformed["mask"]
 
         mask = mask.unsqueeze(0)
 
-        return image,mask
+        return image, mask
